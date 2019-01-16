4ae1b06e16051a5e800061f4e18fea1deae8704e
Ste3et_C0st <ste3et@dicecraft.de>
2019-01-16 18:33:47 +0100

    Step Beckwards old code must be worked correct do not use build #98

--- src/de/Ste3et_C0st/ProtectionLib/main/plugins/fPlotSquared.java
+++ src/de/Ste3et_C0st/ProtectionLib/main/plugins/fPlotSquared.java
@@ -8,9 +8,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
-import com.github.intellectualsites.plotsquared.bukkit.events.PlotClearEvent;
-import com.github.intellectualsites.plotsquared.plot.PlotSquared;
-import com.github.intellectualsites.plotsquared.plot.object.Plot;
+import com.intellectualcrafters.plot.api.PlotAPI;
+import com.intellectualcrafters.plot.object.Plot;
+import com.plotsquared.bukkit.events.PlotClearEvent;
 
 import de.Ste3et_C0st.ProtectionLib.events.RegionClearEvent;
 import de.Ste3et_C0st.ProtectionLib.main.ProtectinObj;
@@ -26,8 +26,8 @@ public class fPlotSquared extends ProtectinObj implements Listener{
 	@EventHandler
 	private void onClear(PlotClearEvent e){
 		World world = Bukkit.getWorld(e.getWorld());
-		com.github.intellectualsites.plotsquared.plot.object.Location sqloc1 = e.getPlot().getAllCorners().get(0);
-		com.github.intellectualsites.plotsquared.plot.object.Location  sqloc2 = e.getPlot().getAllCorners().get(2);
+		com.intellectualcrafters.plot.object.Location sqloc1 = e.getPlot().getAllCorners().get(0);
+		com.intellectualcrafters.plot.object.Location  sqloc2 = e.getPlot().getAllCorners().get(2);
 		Location loc1 = new Location(world, sqloc1.getX(), sqloc1.getY(), sqloc1.getZ());
 		Location  loc2 = new Location(world, sqloc2.getX(), sqloc2.getY(), sqloc2.getZ());
 		RegionClearEvent event = new RegionClearEvent(loc1, loc2);
@@ -46,47 +46,51 @@ public class fPlotSquared extends ProtectinObj implements Listener{
 		return isOwner();
 	}
 	
+	@SuppressWarnings("deprecation")
 	private boolean canBuild(){
 		if(getPlugin()==null){return true;}
-		if(PlotSquared.get().hasPlotArea(getLocation().getWorld().getName())){
-			com.github.intellectualsites.plotsquared.plot.object.Location loc = new com.github.intellectualsites.plotsquared.plot.object.Location(this.getLocation().getWorld().getName(), 
+		PlotAPI pAPI = new PlotAPI();
+		if(pAPI.isPlotWorld(this.getLocation().getWorld())){
+			com.intellectualcrafters.plot.object.Location loc = new com.intellectualcrafters.plot.object.Location(this.getLocation().getWorld().getName(), 
 					(int) this.getLocation().getX(), 
 					(int) this.getLocation().getY(), 
 					(int) this.getLocation().getZ(), 
 					this.getLocation().getYaw(), 
 					this.getLocation().getPitch());
-			if(loc.isPlotArea()) {
-				Plot p = loc.getPlot();
-				if(p != null) {
-					if(p.isAdded(this.getPlayer().getUniqueId())) return true;
-					if(p.isOwner(this.getPlayer().getUniqueId())) return true;
+			if(loc.isPlotArea()){
+				Plot plot = pAPI.getPlot(this.getLocation());
+				if(plot!=null){
+					if(plot.isAdded(this.getPlayer().getUniqueId())) return true;
+					if(plot.isOwner(this.getPlayer().getUniqueId())) return true;
 				}
 				return false;
+			}else{
+				return true;
 			}
-			return true;
-		}else {
+		}else{
 			return true;
 		}
 	}
 	
+	@SuppressWarnings("deprecation")
 	private boolean isOwner(){
 		if(getPlugin()==null){return true;}
-		if(PlotSquared.get().hasPlotArea(getLocation().getWorld().getName())){
-			com.github.intellectualsites.plotsquared.plot.object.Location loc = new com.github.intellectualsites.plotsquared.plot.object.Location(this.getLocation().getWorld().getName(), 
+		PlotAPI pAPI = new PlotAPI();
+		if(pAPI.isPlotWorld(this.getLocation().getWorld())){
+			com.intellectualcrafters.plot.object.Location loc = new com.intellectualcrafters.plot.object.Location(this.getLocation().getWorld().getName(), 
 					(int) this.getLocation().getX(), 
 					(int) this.getLocation().getY(), 
 					(int) this.getLocation().getZ(), 
 					this.getLocation().getYaw(), 
 					this.getLocation().getPitch());
-			if(loc.isPlotArea()) {
-				Plot p = loc.getPlot();
-				if(p != null) {
-					if(p.isOwner(this.getPlayer().getUniqueId())) return true;
-				}
+			if(loc.isPlotArea()){
+				Plot plot = pAPI.getPlot(this.getLocation());
+				if(plot!=null) if(plot.isOwner(this.getPlayer().getUniqueId())) return true;
 				return false;
+			}else{
+				return true;
 			}
-			return true;
-		}else {
+		}else{
 			return true;
 		}
 	}
