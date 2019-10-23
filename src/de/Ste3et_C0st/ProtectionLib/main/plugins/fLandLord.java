899fadab184d20494e7570135f13432b81cd8afe
Alexander Daichendt <alexander.daichendt@tum.de>
2019-10-23 23:51:56 +0200

    Fixed landlord for API changes

--- src/de/Ste3et_C0st/ProtectionLib/main/plugins/fLandLord.java
+++ src/de/Ste3et_C0st/ProtectionLib/main/plugins/fLandLord.java
@@ -1,35 +1,39 @@
 package de.Ste3et_C0st.ProtectionLib.main.plugins;
 
+import biz.princeps.landlord.api.ILandLord;
+import biz.princeps.landlord.api.IOwnedLand;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-
-import biz.princeps.landlord.api.LandLordAPI;
-import biz.princeps.landlord.util.OwnedLand;
 import de.Ste3et_C0st.ProtectionLib.main.protectionObj;
 
 public class fLandLord extends protectionObj {
 
-	public fLandLord(Plugin pl){
-		super(pl);
-	}
-	
-	public boolean canBuild(Player player, Location loc){
-		if(getPlugin()==null){return true;}
-		OwnedLand land = LandLordAPI.getInstance().getLand(loc);
-		if(land==null) return true;
-		if(land.isOwner(player.getUniqueId())){return true;}
-		WorldGuardPlugin wgp = WorldGuardPlugin.inst();
-		if(land.getLand().isMember(wgp.wrapPlayer(player))){return true;}
-		return false;
-	}
-	
-	public boolean isOwner(Player player, Location loc){
-		if(getPlugin()==null){return true;}
-		OwnedLand land = LandLordAPI.getInstance().getLand(loc);
-		if(land==null) return true;
-		return land.isOwner(player.getUniqueId());
-	}
+    public fLandLord(Plugin pl) {
+        super(pl);
+    }
+
+    public boolean canBuild(Player player, Location loc) {
+        if (getPlugin() == null) {
+            return true;
+        }
+        ILandLord plugin = (ILandLord) getPlugin();
+        IOwnedLand land = plugin.getWGManager().getRegion(loc);
+        if (land == null) return true;
+        if (land.isOwner(player.getUniqueId())) {
+            return true;
+        }
+        return land.isFriend(player.getUniqueId());
+    }
+
+    public boolean isOwner(Player player, Location loc) {
+        if (getPlugin() == null) {
+            return true;
+        }
+        ILandLord plugin = (ILandLord) getPlugin();
+        IOwnedLand land = plugin.getWGManager().getRegion(loc);
+        if (land == null) return true;
+        return land.isOwner(player.getUniqueId());
+    }
 }
