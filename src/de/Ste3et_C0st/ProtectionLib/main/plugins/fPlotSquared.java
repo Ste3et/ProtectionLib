3051bf907bf6396ecb4158f2b17f1444da07a2c3
Ste3et_C0st <ste3et@dicecraft.de>
2019-12-20 11:12:45 +0100

    Add PlotDelete event for Plotsquared & legacy

--- src/de/Ste3et_C0st/ProtectionLib/main/plugins/fPlotSquared.java
+++ src/de/Ste3et_C0st/ProtectionLib/main/plugins/fPlotSquared.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
 import com.github.intellectualsites.plotsquared.bukkit.events.PlotClearEvent;
+import com.github.intellectualsites.plotsquared.bukkit.events.PlotDeleteEvent;
 import com.github.intellectualsites.plotsquared.plot.PlotSquared;
 import com.github.intellectualsites.plotsquared.plot.object.Plot;
 import com.github.intellectualsites.plotsquared.plot.object.PlotPlayer;
@@ -26,6 +27,17 @@ public class fPlotSquared extends protectionObj implements Listener{
 	
 	@EventHandler
 	private void onClear(PlotClearEvent e){
+		World world = Bukkit.getWorld(e.getWorld());
+		com.github.intellectualsites.plotsquared.plot.object.Location sqloc1 = e.getPlot().getAllCorners().get(0);
+		com.github.intellectualsites.plotsquared.plot.object.Location  sqloc2 = e.getPlot().getAllCorners().get(2);
+		Location loc1 = new Location(world, sqloc1.getX(), sqloc1.getY(), sqloc1.getZ());
+		Location  loc2 = new Location(world, sqloc2.getX(), sqloc2.getY(), sqloc2.getZ());
+		RegionClearEvent event = new RegionClearEvent(loc1, loc2);
+		Bukkit.getPluginManager().callEvent(event);
+	}
+	
+	@EventHandler
+	private void onClear(PlotDeleteEvent e){
 		World world = Bukkit.getWorld(e.getWorld());
 		com.github.intellectualsites.plotsquared.plot.object.Location sqloc1 = e.getPlot().getAllCorners().get(0);
 		com.github.intellectualsites.plotsquared.plot.object.Location  sqloc2 = e.getPlot().getAllCorners().get(2);
--- src/de/Ste3et_C0st/ProtectionLib/main/plugins/fPlotSquaredLegacy.java
+++ src/de/Ste3et_C0st/ProtectionLib/main/plugins/fPlotSquaredLegacy.java
@@ -12,6 +12,7 @@ import com.intellectualcrafters.plot.api.PlotAPI;
 import com.intellectualcrafters.plot.object.Plot;
 import com.intellectualcrafters.plot.object.PlotPlayer;
 import com.plotsquared.bukkit.events.PlotClearEvent;
+import com.plotsquared.bukkit.events.PlotDeleteEvent;
 
 import de.Ste3et_C0st.ProtectionLib.events.RegionClearEvent;
 import de.Ste3et_C0st.ProtectionLib.main.ProtectionLib;
@@ -34,6 +35,19 @@ public class fPlotSquaredLegacy extends protectionObj implements Listener{
 		RegionClearEvent event = new RegionClearEvent(loc1, loc2);
 		Bukkit.getPluginManager().callEvent(event);
 	}
+	
+	@EventHandler
+	private void onClear(PlotDeleteEvent e){
+		World world = Bukkit.getWorld(e.getWorld());
+		com.intellectualcrafters.plot.object.Location sqloc1 = e.getPlot().getAllCorners().get(0);
+		com.intellectualcrafters.plot.object.Location  sqloc2 = e.getPlot().getAllCorners().get(2);
+		Location loc1 = new Location(world, sqloc1.getX(), sqloc1.getY(), sqloc1.getZ());
+		Location  loc2 = new Location(world, sqloc2.getX(), sqloc2.getY(), sqloc2.getZ());
+		RegionClearEvent event = new RegionClearEvent(loc1, loc2);
+		Bukkit.getPluginManager().callEvent(event);
+	}
+	
+	
 	
 	@SuppressWarnings("deprecation")
 	public boolean canBuild(Player player, Location loc){
