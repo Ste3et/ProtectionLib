5026d72a6201a99769b1105d8cf8fd203bf5aad6
Ste3et_C0st <ste3et@dicecraft.de>
2020-01-13 20:48:59 +0100

    Fix Lands Integration (NoClassDefFoundError)

--- src/de/Ste3et_C0st/ProtectionLib/main/plugins/fLands.java
+++ src/de/Ste3et_C0st/ProtectionLib/main/plugins/fLands.java
@@ -1,36 +1,38 @@
 package de.Ste3et_C0st.ProtectionLib.main.plugins;
 
+import java.util.Objects;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import de.Ste3et_C0st.ProtectionLib.main.ProtectionLib;
 import de.Ste3et_C0st.ProtectionLib.main.protectionObj;
-import me.angeschossen.lands.api.enums.LandsAction;
-import me.angeschossen.lands.api.landsaddons.LandsAddon;
-import me.angeschossen.lands.api.objects.LandChunk;
+import me.angeschossen.lands.api.integration.LandsIntegration;
+import me.angeschossen.lands.api.land.LandChunk;
+import me.angeschossen.lands.api.role.enums.RoleSetting;
 
 public class fLands extends protectionObj{
 
-	private LandsAddon addon;
+	private final LandsIntegration landsAddon;
 	
 	public fLands(Plugin plugin) {
 		super(plugin);
-		this.addon = new LandsAddon(ProtectionLib.getInstance(), true);
+		landsAddon = new LandsIntegration(ProtectionLib.getInstance(), false);
 	}
 
 	@Override
 	public boolean canBuild(Player player, Location loc) {
-		if(this.addon == null) return true;
-		LandChunk chunk = addon.getLandChunk(loc.getChunk());
-		return chunk == null ? true : chunk.canAction(player.getUniqueId().toString(), LandsAction.BLOCK_PLACE);
+		if(Objects.isNull(landsAddon)) return true;
+		LandChunk chunk = landsAddon.getLandChunk(loc);
+		return Objects.isNull(this.landsAddon) ? true : chunk.canAction(player, RoleSetting.BLOCK_PLACE, false);
 	}
 	
 	@Override
 	public boolean isOwner(Player player, Location loc) {
-		if(this.addon == null) return true;
-		LandChunk chunk = addon.getLandChunk(loc.getChunk());
-		return chunk == null ? true : chunk.getOwnerUUID().equalsIgnoreCase(player.getUniqueId().toString());
+		if(Objects.isNull(landsAddon)) return true;
+		LandChunk chunk = landsAddon.getLandChunk(loc);
+		return Objects.isNull(this.landsAddon) ? true : chunk.getOwnerUID().equals(player.getUniqueId());
 	}
 	
 }
