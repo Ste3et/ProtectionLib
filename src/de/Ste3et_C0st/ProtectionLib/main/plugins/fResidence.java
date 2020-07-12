37aff7ba4eb375bdd05df2b4647d6b418ba5b533
Ste3et_C0st <ste3et@dicecraft.de>
2020-07-12 13:50:24 +0200

    Fix residence Wilderness mistake

--- src/de/Ste3et_C0st/ProtectionLib/main/plugins/fResidence.java
+++ src/de/Ste3et_C0st/ProtectionLib/main/plugins/fResidence.java
@@ -6,9 +6,11 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.api.ResidenceApi;
 import com.bekvon.bukkit.residence.containers.Flags;
 import com.bekvon.bukkit.residence.protection.ClaimedResidence;
+import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import com.bekvon.bukkit.residence.protection.ResidencePermissions;
 
 import de.Ste3et_C0st.ProtectionLib.main.protectionObj;
@@ -22,7 +24,10 @@ public class fResidence extends protectionObj {
 	public boolean canBuild(Player player, Location loc){
 		if(getPlugin()==null){return true;}
 		ClaimedResidence residence = ResidenceApi.getResidenceManager().getByLoc(loc);
-		if(residence==null) return true;
+		if(Objects.isNull(residence)) {
+			FlagPermissions perms = Residence.getInstance().getPermsByLoc(loc);
+			return perms.has(Flags.build, true);
+		}
 		ResidencePermissions perms = residence.getPermissions();
 		return perms.playerHas(player, Flags.build, true);
 	}
@@ -38,7 +43,12 @@ public class fResidence extends protectionObj {
 	
 	public boolean isProtectedRegion(Location location) {
 		if(getPlugin()==null){return false;}
+		if(!Flags.build.isGlobalyEnabled()) {
+			return true;
+		}
 		ClaimedResidence residence = ResidenceApi.getResidenceManager().getByLoc(location);
 		return Objects.nonNull(residence);
 	}
+	
+	
 }
