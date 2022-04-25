6a880ec60b8b6fee5235a7503d9b33c3692d2ad8
bloodshot <jdroque@gmail.com>
2022-04-25 00:42:43 -0400

    Fix GriefDefender support.

--- Core/src/main/java/de/Ste3et_C0st/ProtectionLib/main/plugins/fGriefdefenderAPI.java
+++ Core/src/main/java/de/Ste3et_C0st/ProtectionLib/main/plugins/fGriefdefenderAPI.java
@@ -6,63 +6,43 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
-import com.flowpowered.math.vector.Vector3i;
 import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.User;
 import com.griefdefender.api.claim.Claim;
-import com.griefdefender.api.claim.ClaimManager;
+import com.griefdefender.api.event.RemoveClaimEvent;
+import com.griefdefender.lib.flowpowered.math.vector.Vector3i;
+import com.griefdefender.lib.kyori.event.EventSubscriber;
 
 import de.Ste3et_C0st.ProtectionLib.events.RegionClearEvent;
 import de.Ste3et_C0st.ProtectionLib.main.ProtectionConfig;
 import de.Ste3et_C0st.ProtectionLib.main.ProtectionLib;
 
-public class fGriefdefenderAPI extends ProtectionConfig implements Listener {
+public class fGriefdefenderAPI extends ProtectionConfig {
 
 	public fGriefdefenderAPI(Plugin plugin) {
 		super(plugin);
-		Bukkit.getPluginManager().registerEvents(this, ProtectionLib.getInstance());
+		new RemoveClaimEventListener();
 	}
 
 	@Override
 	public void initConfig() {
 		this.addDefault("RegionClearEvent", true);
 	}
-	
-	@EventHandler
-	public void onPlotClear(com.griefdefender.api.event.RemoveClaimEvent removeClaimEvent) {
-		if(getObject("RegionClearEvent") == false) return;
-		if(removeClaimEvent.cancelled() == false) {
-			World world = Bukkit.getWorld(removeClaimEvent.getClaim().getWorldUniqueId());
-			
-			Vector3i min = removeClaimEvent.getClaim().getLesserBoundaryCorner();
-			Vector3i max = removeClaimEvent.getClaim().getGreaterBoundaryCorner();
-			
-			org.bukkit.Location locationMin = new org.bukkit.Location(world, min.getX(), min.getY(), min.getZ());
-			org.bukkit.Location locationMax = new org.bukkit.Location(world, max.getX(), max.getY(), max.getZ());
-			RegionClearEvent event = new RegionClearEvent(locationMin, locationMax);
-			Bukkit.getPluginManager().callEvent(event);
-		}
-	}
-			
 
 	@Override
 	public boolean canBuild(Player player, Location loc) {
-		ClaimManager claimManager = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID());
-		if(Objects.isNull(claimManager)) return true; 
-		Claim claim = claimManager.getClaimAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+		final Claim claim = GriefDefender.getCore().getClaimAt(loc);
 		if(Objects.nonNull(claim) == true) {
-			 return claim.isTrusted(player.getUniqueId());
+			 final User user = GriefDefender.getCore().getUser(player.getUniqueId());
+			 return claim.canBreak(player, loc, user);
 		}
 		return true;
 	}
 
 	@Override
 	public boolean isOwner(Player player, Location loc) {
-		ClaimManager claimManager = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID());
-		if(Objects.isNull(claimManager)) return true; 
-		Claim claim = claimManager.getClaimAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+		final Claim claim = GriefDefender.getCore().getClaimAt(loc);
 		if(Objects.nonNull(claim) == true) {
 			 return claim.getOwnerUniqueId().equals(player.getUniqueId());
 		}
@@ -71,9 +51,28 @@ public class fGriefdefenderAPI extends ProtectionConfig implements Listener {
 
 	@Override
 	public boolean isProtectedRegion(Location loc) {
-		ClaimManager claimManager = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID());
-		if(Objects.isNull(claimManager)) return true;
-		return Objects.nonNull(claimManager.getClaimAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+		final Claim claim = GriefDefender.getCore().getClaimAt(loc);
+		return claim != null && !claim.isWilderness();
 	}
 
+	private class RemoveClaimEventListener {
+
+		public RemoveClaimEventListener() {
+			GriefDefender.getEventManager().getBus().subscribe(RemoveClaimEvent.class, new EventSubscriber<RemoveClaimEvent>() {
+				@Override
+				public void on(RemoveClaimEvent event) throws Throwable {
+					if(getObject("RegionClearEvent") == false) return;
+					final World world = Bukkit.getWorld(event.getClaim().getWorldUniqueId());
+						
+					final Vector3i min = event.getClaim().getLesserBoundaryCorner();
+					final Vector3i max = event.getClaim().getGreaterBoundaryCorner();
+						
+					final Location locationMin = new Location(world, min.getX(), min.getY(), min.getZ());
+					final Location locationMax = new Location(world, max.getX(), max.getY(), max.getZ());
+					RegionClearEvent libEvent = new RegionClearEvent(locationMin, locationMax);
+					Bukkit.getPluginManager().callEvent(libEvent);
+				}
+			});
+		}
+	}
 }
