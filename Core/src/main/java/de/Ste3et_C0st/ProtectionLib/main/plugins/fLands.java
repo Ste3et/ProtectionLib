b83a023a60ab2b1bf3908cd5b80b9d717a8c0ae4
Ste3et_C0st <ste3et@dicecraft.de>
2023-02-18 18:09:02 +0100

    try to fix Lands integration

--- Core/src/main/java/de/Ste3et_C0st/ProtectionLib/main/plugins/fLands.java
+++ Core/src/main/java/de/Ste3et_C0st/ProtectionLib/main/plugins/fLands.java
@@ -3,14 +3,15 @@ package de.Ste3et_C0st.ProtectionLib.main.plugins;
 import java.util.Objects;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import de.Ste3et_C0st.ProtectionLib.main.ProtectionLib;
 import de.Ste3et_C0st.ProtectionLib.main.protectionObj;
-import me.angeschossen.lands.api.integration.LandsIntegration;
-import me.angeschossen.lands.api.land.Area;
+import me.angeschossen.lands.api.LandsIntegration;
+import me.angeschossen.lands.api.flags.type.Flags;
 import me.angeschossen.lands.api.land.Land;
-import me.angeschossen.lands.api.role.enums.RoleSetting;
+import me.angeschossen.lands.api.land.LandWorld;
 
 public class fLands extends protectionObj{
 
@@ -18,26 +19,25 @@ public class fLands extends protectionObj{
 	
 	public fLands(Plugin plugin) {
 		super(plugin);
-		landsAddon = new LandsIntegration(ProtectionLib.getInstance());
+		landsAddon = LandsIntegration.of(ProtectionLib.getInstance());
 	}
 
 	@Override
 	public boolean canBuild(Player player, Location loc) {
 		if(Objects.isNull(landsAddon)) return true;
-		
-	    Area chunk = landsAddon.getAreaByLoc(loc);
-		return Objects.isNull(chunk) ? true : chunk.canSetting(player, RoleSetting.BLOCK_PLACE, false);
+		final LandWorld world = landsAddon.getWorld(loc.getWorld());
+		return Objects.isNull(world) ? true : world.hasRoleFlag(player, loc,  Flags.BLOCK_PLACE, Material.STONE, isEnabled());
 	}
 	
 	@Override
 	public boolean isOwner(Player player, Location loc) {
 		if(Objects.isNull(landsAddon)) return true;
-		Land chunk = landsAddon.getLand(loc);
+		Land chunk = landsAddon.getLandByChunk(loc.getWorld(), loc.getBlockX() >> 4, loc.getBlockZ() >> 4);
 		return Objects.isNull(chunk) ? true : chunk.getOwnerUID().equals(player.getUniqueId());
 	}
 	
 	public boolean isProtectedRegion(Location location) {
-		Land chunk = landsAddon.getLand(location);
+		Land chunk = landsAddon.getLandByChunk(location.getWorld(), location.getBlockX() >> 4, location.getBlockZ() >> 4);
 		return Objects.nonNull(chunk);
 	}
 }
